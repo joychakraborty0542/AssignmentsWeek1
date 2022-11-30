d400988ebc6e44fe4b4e51ae2298d802b4646315
joychakraborty0542 <joyckb30@gmail.com>
2022-11-30 13:53:08 +0000

    work

---
+++ src/assignmentSolution2/Movie.java
@@ -0,0 +1,30 @@
+package assignmentSolution2;
+
+public class Movie {
+	private int  movid;
+	
+	public int getMovid() {
+		return movid;
+	}
+
+	public Movie(int movid, int price) {
+		//super();
+		this.movid = movid;
+		Price = price;
+	}
+
+	public void setMovid(int movid) {
+		this.movid = movid;
+	}
+
+	public int getPrice() {
+		return Price;
+	}
+
+	public void setPrice(int price) {
+		Price = price;
+	}
+
+	private int Price;
+
+}
